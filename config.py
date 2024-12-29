68324311627b7d2519eb9f70591c0a015cfc0e81
Sanket117 <nsksanketsatpute@gmail.com>
2024-12-30 03:30:38 +0530

    final commit

--- config.py
+++ config.py
@@ -2,11 +2,11 @@
 
 MODEL_CONFIGS = {
     'yolo': {
-        'drive_url': 'https://drive.google.com/uc?id=1p-Y7rd0FfjNnv_jewCi7ZjXH3T-qtyAa',
+        'drive_url': 'https://drive.google.com/file/d/1p-Y7rd0FfjNnv_jewCi7ZjXH3T-qtyAa',
         'local_path': 'weights/best.pt'
     },
     'florence': {
-        'drive_url': 'https://drive.google.com/uc?id=1hUCqZ3X8mcM-KcwWFjcsFg7PA0hUvE3k',
-        'local_path': 'weights/icon_caption_florence'
+        'drive_url': 'https://drive.google.com/file/d/1hUCqZ3X8mcM-KcwWFjcsFg7PA0hUvE3k',
+        'local_path': 'weights/icon_caption_florence/model.safetensors'
     }
 }
