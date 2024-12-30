7237ecc3cc795bf18bc6146e65f2ed67f3788104
Sanket117 <nsksanketsatpute@gmail.com>
2024-12-30 06:05:46 +0530

    final commit

--- config.py
+++ config.py
@@ -5,7 +5,9 @@ MODEL_CONFIGS = {
     },
     'florence': {
         'drive_url': 'https://drive.google.com/uc?id=1hUCqZ3X8mcM-KcwWFjcsFg7PA0hUvE3k',
-        'local_path': 'weights/icon_caption_florence/model.safetensors',
+        'local_path': 'weights/icon_caption_florence',
+        
+        
         'pytorch_model_url': 'https://drive.google.com/file/d/1PiNJSTORmAbaOMpOv_wa6djuVLbN3gh9',  # Replace with actual file ID
         'pytorch_model_path': 'weights/pytorch_model.bin'  # Local path to save pytorch_model.bin
     }
